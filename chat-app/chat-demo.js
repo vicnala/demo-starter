a2a8a871d2cd0893b4d594af11d3b66caf68d3ec
Víctor Navarro <vicnala@gmail.com>
2015-09-16 13:23:14 +0200

    Update chat-demo.js

--- chat-app/chat-demo.js
+++ chat-app/chat-demo.js
@@ -2,16 +2,45 @@ Messages = new Mongo.Collection("msgs");
 
 if (Meteor.isServer) {
   // This code only runs on the server
+  console.log("I'm the server");
+  /*
   Meteor.publish("messages", function () {
     return Messages.find({}, {sort: {createdAt: -1}, limit: 5});
   });
+  */
 }
 
 if (Meteor.isClient) {
   // This code only runs on the client
+  /*
   Meteor.subscribe("messages");
+  */
+  console.log("I'm the client");
+
+  Template.body.helpers({
+    recentMessages: function () {
+      return Messages.find({}, {sort: {createdAt: 1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-message": function (event) {
+      var text = event.target.text.value;
+
+      console.log(text);
+      //Meteor.call("sendMessage", text);
+
+      event.target.text.value = "";
+      return false;
+    }
+  });
+
+  // Accounts.ui.config({
+  //   passwordSignupFields: "USERNAME_ONLY"
+  // });
 }
 
+/*
 Meteor.methods({
   sendMessage: function (message) {
     // if (! Meteor.userId()) {
@@ -25,27 +54,4 @@ Meteor.methods({
     });
   }
 });
-
-if (Meteor.isClient) {
-  Template.body.helpers({
-    recentMessages: function () {
-      return Messages.find({}, {sort: {createdAt: 1}});
-    }
-  });
-
-  Template.body.events({
-    "submit .new-message": function (event) {
-      var text = event.target.text.value;
-
-      Meteor.call("sendMessage", text);
-
-      event.target.text.value = "";
-      return false;
-    }
-  });
-
-  // Accounts.ui.config({
-  //   passwordSignupFields: "USERNAME_ONLY"
-  // });
-}
-
+*/
